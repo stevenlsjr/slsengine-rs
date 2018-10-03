4ef77679b4e4e4bb92a045761a1fae1ecfedfe43
Steve Shea <stevenlsjr@gmail.com>
2018-10-03 17:52:30 -0400

    working on vulkan launcher

--- src/bin/vk_run.rs
+++ src/bin/vk_run.rs
@@ -1,13 +1,28 @@
-
 extern crate slsengine;
+
 use slsengine::sdl_platform::*;
 
-fn main(){
-    let plt = platform().with_window_size(640, 480)
-        .with_window_title("Hello rust!")
-        .with_vulkan()
-        .build().unwrap();
+fn main() {
+    //    let plt = platform().with_window_size(640, 480)
+//        .with_window_title("Hello rust!")
+//        .with_vulkan()
+//        .build().unwrap();
+    use std::sync::mpsc;
+    use std::thread;
+
+    let (sender, receiver) = mpsc::channel();
+    thread::spawn(move || {
+        let val = "hi".to_owned();
+        sender.send(val).unwrap();
+    });
 
 
+    let received = receiver.recv().unwrap();
+    println!("got {:?}", received);
 
+    use std::io::{self, Read};
+    if cfg!(windows) {
+        let mut buffer = [0; 1];
+        io::stdin().read(&mut buffer);
+    }
 }
