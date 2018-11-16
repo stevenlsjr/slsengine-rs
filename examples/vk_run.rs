94e1a8c049228c29969b16203f0c57667d292824
Steven Shea <stevenlsjr@gmail.com>
2018-11-16 10:14:36 -0500

    refactored vulkan context creation into a VulkanRender object

--- examples/vk_run.rs
+++ examples/vk_run.rs
@@ -39,19 +39,63 @@ impl PlatformBuilderHooks for VulkanPlatformHooks {
 }
 extern crate cgmath;
 use cgmath::prelude::*;
-struct VulkanRenderer {
+use std::sync::Arc;
+use vulkano::instance::Instance;
+use vulkano::swapchain::Surface;
+use std::rc::Rc;
+use vulkano::device::Device;
+
+#[derive(Debug)]
+pub struct VulkanRenderer {
     camera: RefCell<Camera>,
+    instance: Arc<Instance>,
+    device: Arc<Device>,
+    queues: VulkanQueues,
+    surface: Arc<Surface<Rc<WindowContext>>>
+
 }
 impl VulkanRenderer {
-    fn new() -> Self {
-        VulkanRenderer {
+    fn new(window: &Window) -> Result<Self, VkContextError> {
+        use vulkano::instance::{Instance, PhysicalDevice, RawInstanceExtensions};
+        use vulkano::swapchain::Surface;
+        use vulkano::VulkanObject;
+        let instance_extensions =
+            window.vulkan_instance_extensions().unwrap();
+        let raw_instance_extensions = RawInstanceExtensions::new(
+            instance_extensions
+                .iter()
+                .map(|&v| CString::new(v).unwrap()),
+        );
+        let layers = vec!["VK_LAYER_LUNARG_standard_validation"];
+        let instance = Instance::new(None, raw_instance_extensions, layers)
+            .expect("failed to create vulkan instance");
+        let surface: Arc<SdlSurface> = {
+            let handle = window
+                .vulkan_create_surface(instance.internal_object())
+                .map_err(|_| VkContextError::Surface)?;
+            unsafe {
+                Arc::new(Surface::from_raw_surface(
+                    instance.clone(),
+                    handle,
+                    window.context().clone(),
+                ))
+            }
+        };
+        let physical_device = pick_physical_device(&instance).unwrap();
+        let (device, queues) = create_device(&physical_device, &surface).unwrap();
+
+        Ok(VulkanRenderer {
             camera: RefCell::new(Camera::new(cgmath::PerspectiveFov {
                 fovy: cgmath::Deg(40.0).into(),
                 aspect: 1.0,
                 near: 0.1,
                 far: 100.0,
             })),
-        }
+            instance,
+            device,
+            queues,
+            surface,
+        })
     }
 }
 
@@ -64,52 +108,27 @@ impl slsengine::renderer::Renderer for VulkanRenderer {
 fn main() {
     use slsengine::game;
     use std::time::{Duration, Instant};
-    use vulkano::instance::{Instance, RawInstanceExtensions, PhysicalDevice};
-    use vulkano::swapchain::Surface;
-    use vulkano::VulkanObject;
+
     let platform = platform().build(&VulkanPlatformHooks).unwrap();
 
-    let instance_extensions =
-        platform.window.vulkan_instance_extensions().unwrap();
-    let raw_instance_extensions = RawInstanceExtensions::new(
-        instance_extensions
-            .iter()
-            .map(|&v| CString::new(v).unwrap()),
-    );
-    let layers = vec!["VK_LAYER_LUNARG_standard_validation"];
-    let instance = Instance::new(None, raw_instance_extensions, layers)
-        .expect("failed to create vulkan instance");
-    let surface = {
-        let handle = platform
-            .window
-            .vulkan_create_surface(instance.internal_object())
-            .unwrap();
-        unsafe {
-            Surface::from_raw_surface(
-                instance.clone(),
-                handle,
-                platform.window.context(),
-            )
-        }
-    };
-    let physical_device = pick_physical_device(&instance).unwrap();
-
-    let mut loop_state = slsengine::MainLoopState::new();
-    let mut timer = game::Timer::new(Duration::from_millis(100 / 6));
-    let mut renderer = VulkanRenderer::new();
-    loop_state.is_running = true;
-    while loop_state.is_running {
-        loop_state.handle_events(
-            &platform.window,
-            platform.event_pump.borrow_mut().poll_iter(),
-            &mut renderer,
-        );
-        let game::Tick { delta: _delta, .. } = timer.tick();
-
-        let ticks = Instant::now().duration_since(timer.start_instant());
-        let theta = game::duration_as_f64(ticks);
 
 
-
-    }
+    //    let mut loop_state = slsengine::MainLoopState::new();
+    //    let mut timer = game::Timer::new(Duration::from_millis(100 / 6));
+    //    let mut renderer = VulkanRenderer::new();
+    //    loop_state.is_running = true;
+    //    while loop_state.is_running {
+    //        loop_state.handle_events(
+    //            &platform.window,
+    //            platform.event_pump.borrow_mut().poll_iter(),
+    //            &mut renderer,
+    //        );
+    //        let game::Tick { delta: _delta, .. } = timer.tick();
+    //
+    //        let ticks = Instant::now().duration_since(timer.start_instant());
+    //        let theta = game::duration_as_f64(ticks);
+    //
+    //
+    //
+    //    }
 }
