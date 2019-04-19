fed85d4139424175264272251681bec4734ee5ab
Steve Shea <stevenlsjr@gmail.com>
2019-04-19 15:44:58 -0400

    Created separate module for application type

---
+++ src/application.rs
@@ -0,0 +1,97 @@
+use std::any::Any;
+use std::fmt::{Debug, Display};
+
+use sdl2::event::EventType::AppDidEnterBackground;
+
+use crate::game::main_loop::FrameTick;
+use crate::game::WorldManager;
+use crate::MainLoopState;
+///!
+///! Application lifecycle manager
+
+use crate::renderer::Renderer;
+use crate::sdl_platform::{Platform, RenderBackend};
+use crate::sdl_platform::RenderBackend::Undefined;
+use specs::World;
+
+pub struct ApplicationBuilder {
+    preferred_backend: RenderBackend,
+}
+
+impl ApplicationBuilder {
+    pub fn new() -> Self {
+        ApplicationBuilder { preferred_backend: Undefined }
+    }
+
+    fn build<R: Renderer>(self) -> Result<Application<R>, failure::Error> {
+        unimplemented!()
+    }
+}
+
+
+pub fn application() -> ApplicationBuilder {
+    ApplicationBuilder::new()
+}
+
+
+#[derive(Debug)]
+pub struct Application<R: Renderer> {
+    pub platform: Platform,
+    pub renderer: R,
+    pub main_loop: MainLoopState,
+    pub world_manager: WorldManager,
+}
+
+impl<R: Renderer> Application<R> {
+
+
+    #[inline]
+    pub fn world(&self) -> &World {
+        self.world_manager.world()
+    }
+
+    #[inline]
+    pub fn world_mut(&mut self) -> &mut World {
+        self.world_manager.world_mut()
+    }
+    pub fn run(self) -> Result<(), i32> {
+        self.setup().map_err(|e| {
+            eprintln!("app startup error! {:?}", e);
+            -1
+        })?;
+        while self.main_loop.is_running() {
+            let frame = self.world_manager.world_mut().write_resource::<FrameTick>();
+            *frame = self.main_loop.tick_frame();
+            {
+                self.main_loop.handle_events(&self.platform.window,
+                                             &self.platform.event_pump,
+                                             &self.renderer,
+                                             &mut self.world_manager);
+            }
+
+        }
+
+        Ok(())
+    }
+    fn setup(&mut self) -> Result<(), failure::Error> {
+        self.main_loop.start();
+        let frame = self.main_loop.tick_frame();
+        self.world_manager.world_mut().add_resource(frame);
+        Ok(())
+    }
+}
+
+#[derive(Fail, Debug)]
+pub enum AppError {
+    #[fail(display = "App error: '{}'", _0)]
+    Other(failure::Error),
+}
+
+
+impl AppError {
+    pub fn from_message<D: Display + Debug + Send + Sync + Sized + 'static>(
+        message: D,
+    ) -> AppError {
+        AppError::Other(failure::err_msg(message))
+    }
+}
--- src/bin/slsengine_run.rs
+++ src/bin/slsengine_run.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)]
 
 use hibitset::BitSet;
+
 #[cfg(feature = "backend-vulkan")]
 use slsengine::renderer::backend_vk;
 use slsengine::{
     game::*,
     renderer::*,
+    application::*,
     sdl_platform::{self, Platform},
 };
 
@@ -16,26 +18,14 @@ use slsengine::{
     sdl_platform::{OpenGLVersion, OpenGLVersion::GL45},
 };
 
-use slsengine::game;
-use slsengine::game::resource::DeltaTime;
-#[cfg(feature = "backend-gl")]
-use specs::prelude::*;
-
-struct App<R: Renderer> {
-    platform: Platform,
-    renderer: R,
-    main_loop: MainLoopState,
-    world: WorldManager<R>,
-}
-
 #[cfg(feature = "backend-vulkan")]
-fn setup_vk() -> Result<App<backend_vk::VulkanRenderer>, failure::Error> {
+fn setup_vk() -> Result<Application<backend_vk::VulkanRenderer>, failure::Error> {
     let platform =
         sdl_platform::platform().build(&backend_vk::VulkanPlatformHooks)?;
     let renderer = backend_vk::VulkanRenderer::new(&platform.window)?;
     let main_loop = MainLoopState::new();
     let world = WorldManager::new(&renderer);
-    Ok(App {
+    Ok(Application {
         platform,
         renderer,
         main_loop,
@@ -44,52 +34,28 @@ fn setup_vk() -> Result<App<backend_vk::VulkanRenderer>, failure::Error> {
 }
 
 #[cfg(feature = "backend-gl")]
-fn setup_gl() -> Result<App<backend_gl::GlRenderer>, failure::Error> {
+fn setup_gl() -> Result<Application<backend_gl::GlRenderer>, failure::Error> {
     let (platform, gl) = sdl_platform::platform()
         .with_opengl(OpenGLVersion::GL41)
         .build_gl()?;
     let renderer = backend_gl::GlRenderer::new(&platform.window)?;
     let main_loop = MainLoopState::new();
     let world = WorldManager::new(&renderer);
-    Ok(App {
+    Ok(Application {
         platform,
         renderer,
         main_loop,
-        world,
+        world_manager: world,
     })
 }
 
 fn main() -> Result<(), i32> {
+
     let mut app = setup_gl().map_err(|e| {
         eprintln!("app error: {}", e);
         1
     })?;
 
-    app.main_loop.start();
-    let mut update_dispatch = DispatcherBuilder::new().build();
-    while app.main_loop.is_running() {
-        {
-            let App {
-                ref mut main_loop,
-                ref renderer,
-                ref mut world,
-                ref mut platform,
-                ..
-            } = app;
-            main_loop.handle_events(
-                &mut platform.window,
-                &mut platform.event_pump,
-                renderer,
-                world,
-            );
-        }
-        let frame = app.main_loop.tick_frame();
-        let entity_world = app.world.world();
-        {
-            let mut dt = entity_world.write_resource::<DeltaTime>();
-            dt.0 = frame.delta;
-        }
-    }
+    app.run()
 
-    Ok(())
 }
