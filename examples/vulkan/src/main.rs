ce2640984308fd0e5addaede6aed2708ad5b2c6d
Steve Shea <stevenlsjr@gmail.com>
2019-06-13 17:31:04 -0400

    fixed some stuff for vulka

--- examples/vulkan/src/main.rs
+++ examples/vulkan/src/main.rs
@@ -1,3 +1,7 @@
+use slsengine;
+use slsengine_backend_vk as bvk;
+use slsengine_platform_sdl as plt;
+
 fn main() {
-    println!("Hello, world!");
+    let platform = plt::platform().build_vk().expect();
 }
